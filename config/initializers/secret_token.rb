41e2599f36285e21d52c3611b1044bc896dbb393
Ian Chilton <ian@ichilton.co.uk>
2013-11-16 09:32:43 +0000

    Initial commit after running rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailingListManager::Application.config.secret_key_base = '7723d1b4cc4e7a75c4726fad1a0543cf5286b0f8d963bb951343e1f904fbb5ba3b07abcb1f62643142a7be0d638939283ecfe2b42773899dc520f674a17b30ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailingListManager::Application.config.session_store :cookie_store, key: '_mailing_list_manager_session'
